2a84d771374be9c3607381dec07c01d1b93395bc
ChungHoon <chad6027@gmail.com>
2020-12-30 20:49:32 +0900

    201230

--- practice.py
+++ practice.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html')
+
+
+@app.route('/post', methods=['POST'])
+def post():
+    if request.method == 'POST':
+        data = request.form['data']
+        return data
+    else:
+        return render_template('home.html')
 
 
 if __name__ == '__main__':
