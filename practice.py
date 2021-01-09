d5c482c05dac14297e29576970c19db26b18247d
ChungHoon <chad6027@gmail.com>
2021-01-09 16:26:16 +0900

    .gitignore

--- practice.py
+++ practice.py
@@ -1,10 +1,26 @@
+import pyrebase
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+config = {
+    "apiKey": "AIzaSyAqAZIyg2YSVvXCgtqI0b4IGYfp-R88mH8",
+    "authDomain": "winter-80fa9.firebaseapp.com",
+    "databaseURL": "https://winter-80fa9-default-rtdb.firebaseio.com",
+    "projectId": "winter-80fa9",
+    "storageBucket": "winter-80fa9.appspot.com",
+    "messagingSenderId": "1047783723358",
+    "appId": "1:1047783723358:web:0e0aaaae52df428877bfcf",
+    "measurementId": "G-XFTNSE9Z2V"
+  };
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
 @app.route('/')
 def hello_world():
+    db.child('test').update({"company" : "google"})
+    db.child('winter').update({"friend": "윤석"})
     return render_template('home.html')
 
 
