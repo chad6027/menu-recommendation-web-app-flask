05dd7dd8130376c9e89e39dd4449c5ddbad3f2ab
ChungHoon <chad6027@gmail.com>
2021-01-10 01:05:43 +0900

    FrontEnd

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
 import pyrebase
+"""import sqlite3"""
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -12,16 +13,31 @@ config = {
     "messagingSenderId": "1047783723358",
     "appId": "1:1047783723358:web:0e0aaaae52df428877bfcf",
     "measurementId": "G-XFTNSE9Z2V"
-  };
+};
 
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
+cur_qt = 0
+qt = [
+    '한식 어때?',
+    '다이어트중이야?',
+    '매운게 끌려?',
+    '누구랑 먹어?',
+    '술은? 안드시게? 이걸 참아?'
+]
+choice = {0: ['O', 'X'], 1: ['O', 'X'], 2: ['O', 'X'], 3: ['X', '혼자', '애인', ' 그외'], 4: ['X', '맥주', '소주']}
+
 
 @app.route('/')
 def hello_world():
-    db.child('test').update({"company" : "google"})
-    db.child('winter').update({"friend": "윤석"})
     return render_template('home.html')
+
+
+@app.route('/question')
+def question():
+    global cur_qt
+    cur_qt = (cur_qt + 1) % 5
+    return render_template('question.html', question=qt[cur_qt], choices=choice[cur_qt])
 
 
 @app.route('/post', methods=['POST'])
