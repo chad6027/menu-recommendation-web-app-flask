0ec6839c8560e8be19ee1e88ddd71a32d55c2ea5
ChungHoon <chad6027@gmail.com>
2021-01-25 22:31:58 +0900

    210125

--- modules/bayes.py
+++ modules/bayes.py
@@ -7,36 +7,36 @@ udo_drinking = [0.9, 0.1, 0.9, 0.9, 0.1]
 key = {1, 2, 3, 4, 5}
 
 
-def updatePrior(prior, udo, question):  # bayes정리를 이용하여 사전확률을 갱신한다.
+def updatePrior(prior, udo):  # bayes정리를 이용하여 사전확률을 갱신한다.
     up, down = 0, 0
-    print(question, end="")
     ans1 = input("(y or n) : ")
     result = []  # 초기화해야 함수를 반복적으로 사용 가능
 
-    if ans1 == 'y':
-        for k, v in prior.items():  # 사후 확률 계산
-            up = udo[k - 1] * v  # 분자 계산
-            for ke, va in prior.items():  # 분모 계산
-                down += udo[ke - 1] * va
+    for index, value in enumerate(prior):
+        up = udo[index] * value  # 분자 계산
+        for in_index, in_value in enumerate(prior):  # 분모 계산
+            down += udo[in_index] * in_value
 
-            postValue = round(up / down, 4)  # postValue : 사후확률, 소수점 아래 조절
-            result.append(postValue)  # list result에 값 삽입
-            up, down = 0, 0
-    else:
-        for k, v in prior.items():  # 사후 확률 계산
-            up = (1 - udo[k - 1]) * v  # 분자 계산
-            for ke, va in prior.items():  # 분모 계산
-                down += (1 - udo[ke - 1]) * va
+        postValue = round(up / down, 4)  # postValue : 사후확률, 소수점 아래 조절
+        result.append(postValue)  # list result에 값 삽입
+        up, down = 0, 0
 
-            postValue = round(up / down, 4)
-            result.append(postValue)
-            up, down = 0, 0
+    return result
 
-    dict_result = dict(zip(key, result))  # 두 리스트를 딕셔너리 형태로 변환
-    print(dict_result)
+    # if ans1 == 'y':
+    #     for k, v in prior.items():  # 사후 확률 계산
+    #         up = udo[k - 1] * v  # 분자 계산
+    #         for ke, va in prior.items():  # 분모 계산
+    #             down += udo[ke - 1] * va
+    #
+    #         postValue = round(up / down, 4)  # postValue : 사후확률, 소수점 아래 조절
+    #         result.append(postValue)  # list result에 값 삽입
+    #         up, down = 0, 0
 
-    return dict_result
-
+    # dict_result = dict(zip(key, result))  # 두 리스트를 딕셔너리 형태로 변환
+    # print(dict_result)
+    #
+    # return dict_result
 
 prior = updatePrior(prior, udo_kor, "한식 어때")
 
--- practice.py
+++ practice.py
@@ -4,11 +4,27 @@ import random
 from modules import db
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 
+
 app = Flask(__name__)
 
 mysql = db.Database()
-dict_session_question = dict()
 app.secret_key = os.urandom(16)
+
+
+def updatePrior(prior, udo):  # bayes정리를 이용하여 사전확률을 갱신한다.
+    up, down = 0, 0
+    result = []  # 초기화해야 함수를 반복적으로 사용 가능
+
+    for index, value in enumerate(prior):
+        up = udo[index] * value  # 분자 계산
+        for in_index, in_value in enumerate(prior):  # 분모 계산
+            down += udo[in_index] * in_value
+
+        postValue = round(up / down, 5)  # postValue : 사후확률, 소수점 아래 조절
+        result.append(postValue)  # list result에 값 삽입
+        up, down = 0, 0
+
+    return result
 
 
 def get_random_key():
@@ -37,16 +53,21 @@ def play():
         new_session = get_random_key()
 
     # session 안에 데이터로 음식들의 prior값을 넣으면 나중에 처리하기 쉬워질 것 같다.
-    session[new_session] = 0
+    session[new_session] = dict()
+    session[new_session]['cur'] = 0
 
     # random question order
-    rand_question = mysql.executeAll("select que_no from qna ORDER BY rand()")
+    rand_question = mysql.executeAll("SELECT que_no FROM qna ORDER BY rand()")
 
     # list 안에 dict 형태로 SELECT 결과가 저장되어있는 것을 value 만 갖고와서 따로 list 로 저장
     rand_question = [value['que_no'] for value in rand_question]
-    print("Session requested : " + new_session)
-    print(rand_question)
-    dict_session_question[new_session] = rand_question
+    session[new_session]['question_order_cur'] = 0
+    session[new_session]['question_order'] = rand_question
+
+    #사전 확률 구해서 저장
+    prior_probability = mysql.executeAll("SELECT prior FROM food_prior")
+    prior_probability = [value['prior'] for value in prior_probability]
+    session[new_session]['prior'] = prior_probability
 
     return render_template('question.html', key=new_session)
 
@@ -54,13 +75,17 @@ def play():
 @app.route('/ajax', methods=['POST'])
 def ajax():
     data = request.get_json()
-    key = data['session']
+    session_key = data['session']
+    ans = data['answer']
     answer_list = list()
 
-    next_idx = dict_session_question[key][session[key]]
-    session[key] += 1
+    session.modified = True
+    session[session_key]['question_order_cur'] += 1
+    next_idx = session[session_key]['question_order_cur']
+    next_qna_idx = session[session_key]['question_order'][next_idx]
 
-    query = "SELECT * FROM qna WHERE que_no = " + str(next_idx)
+    query = "SELECT * FROM qna WHERE que_no = " + str(next_qna_idx)
+
     next_qna = mysql.executeOne(query)
 
     #ans_v*가 null 이면 버튼 추가X
@@ -75,7 +100,16 @@ def ajax():
     print("next idx : " + str(next_idx))
     print(answer_list)
 
+    if ans != 0:
+        table_name = next_qna['udo_name']
+        query = "SELECT " + ans + " FROM " + table_name
+        udo = mysql.executeAll(query)
+        udo = [value[ans] for value in udo]
+
+        session[session_key]['prior'] = updatePrior(session[session_key]['prior'], udo)
+
     return jsonify(result=next_qna['que'], answer=answer_list)
+
 
 # @app.route('/result', methods=['POST'])
 # def result():
